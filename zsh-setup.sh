bca9323d8bdeb1235cd13c63b6dd74cda5d7040c
Tobias Nolz <tobias.nolz@gmx.at>
2021-10-22 12:07:02 +0200

    Fixed bugs

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,20 +1,21 @@
 git config --global core.autocrlf false
 git config --global core.eol lf
 
-git fetch origin
-git reset --hard origin/master
+#git fetch origin
+#git reset --hard origin/master
 
 #Oh my Zsh
+rm -rf /home/main/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #PowerLevel10K
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 #Zsh Syntax Highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 #Zsh suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sudo apt update && sudo apt install ruby-full
 sudo gem install colorls
@@ -22,5 +23,3 @@ sudo gem install colorls
 cp .zshrc ~/.zshrc
 cp .vimrc ~/.vimrc
 cp .p10k.zsh ~/.p10k.zsh
-
-source ~/.zshrc
