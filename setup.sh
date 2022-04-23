db60f0a0ee454fafa10f5700cf9138d4cda7f2a7
Tobias Nolz <tobias.nolz@gmx.at>
2022-04-23 22:29:32 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -40,6 +40,12 @@ sudo chsh $USER -s /bin/zsh
 # symlink all dotfiles
 rm ~/.zshrc ~/.vimrc ~/.p10k.zsh
 stow */
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+vim +'PlugInstall --sync' +qa
+
 exec /bin/zsh
 
 echo "installation done"
